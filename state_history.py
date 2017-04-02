0ce4e88c1466682e55679fcbd1abdd915d1b6cd5
rurush47 <olekrurarz@gmail.com>
2017-04-02 22:09:49 +0200

    Refactoring board functions to statics WIP

--- state_history.py
+++ state_history.py
@@ -1,12 +1,11 @@
-import copy
-
-
 class StateHistory:
     def __init__(self):
         self.states = []
 
     def add_state(self, state):
         self.states.append(state)
+        if len(self.states) > 2:
+            self.states.pop(0)
 
     def is_already_in_history(self, state):
         if state in self.states:
@@ -16,3 +15,6 @@ class StateHistory:
 
     def get_last_state(self):
         return self.states[-1]
+
+    def get_state_list(self):
+        return self.states
